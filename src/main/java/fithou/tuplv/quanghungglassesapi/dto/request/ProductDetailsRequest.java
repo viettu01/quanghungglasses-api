3436f70b065b1c9f56b8e0758c7e651570602d20
viettu01 <tupham1120@gmail.com>
2024-02-22 17:31:46 +0700

    clean code

--- src/main/java/fithou/tuplv/quanghungglassesapi/dto/request/ProductDetailsRequest.java
+++ src/main/java/fithou/tuplv/quanghungglassesapi/dto/request/ProductDetailsRequest.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
